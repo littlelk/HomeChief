cae274ecb8e8de23db68bd9cdb01180bdef277ba
milerliu <milerliu@tencent.com>
2026-06-20 09:47:31 +0800

    feat: add HomeChief creation flows

--- miniprogram/pages/create-life/create-life.js
+++ miniprogram/pages/create-life/create-life.js
@@ -1,5 +1,38 @@
 Page({
   data: {
-    title: '写生活',
+    recipeId: '',
+    body: '',
+    photos: [],
+    tags: ['生活', '饭菜'],
+    uploadError: '',
+  },
+
+  onLoad(query) {
+    this.setData({ recipeId: query.recipeId || '' })
+  },
+
+  onBodyInput(event) {
+    this.setData({ body: event.detail.value })
+  },
+
+  choosePhotos() {
+    wx.chooseMedia({
+      count: 9,
+      mediaType: ['image'],
+      success: (res) => {
+        const photos = res.tempFiles.map((file) => file.tempFilePath)
+        this.setData({ photos: this.data.photos.concat(photos), uploadError: '' })
+      },
+      fail: () => this.setData({ uploadError: '图片上传失败，请重试。' }),
+    })
+  },
+
+  publish() {
+    if (!this.data.body.trim() && this.data.photos.length === 0) {
+      wx.showToast({ title: '写点文字或传张照片', icon: 'none' })
+      return
+    }
+    wx.showToast({ title: '已发布', icon: 'success' })
+    setTimeout(() => wx.switchTab({ url: '/pages/feed/feed' }), 500)
   },
 })
--- miniprogram/pages/create-recipe/create-recipe.js
+++ miniprogram/pages/create-recipe/create-recipe.js
@@ -1,5 +1,81 @@
+const { findRecipeById } = require('../../data/homechief')
+
 Page({
   data: {
-    title: '记菜谱',
+    id: '',
+    name: '',
+    note: '',
+    tags: ['晚餐', '快手菜'],
+    cover: '',
+    ingredientsText: '',
+    steps: [{ title: '', body: '', image: '' }],
+    uploadError: '',
+  },
+
+  onLoad(query) {
+    if (!query.id) return
+    const recipe = findRecipeById(query.id)
+    if (!recipe) return
+    this.setData({
+      id: recipe.id,
+      name: recipe.name,
+      note: recipe.note,
+      tags: recipe.tags,
+      cover: recipe.cover,
+      ingredientsText: recipe.ingredients.join('\n'),
+      steps: recipe.steps,
+    })
+  },
+
+  onNameInput(event) {
+    this.setData({ name: event.detail.value })
+  },
+
+  onNoteInput(event) {
+    this.setData({ note: event.detail.value })
+  },
+
+  onIngredientsInput(event) {
+    this.setData({ ingredientsText: event.detail.value })
+  },
+
+  onStepTitleInput(event) {
+    const index = event.currentTarget.dataset.index
+    const steps = this.data.steps
+    steps[index].title = event.detail.value
+    this.setData({ steps })
+  },
+
+  onStepBodyInput(event) {
+    const index = event.currentTarget.dataset.index
+    const steps = this.data.steps
+    steps[index].body = event.detail.value
+    this.setData({ steps })
+  },
+
+  addStep() {
+    this.setData({ steps: this.data.steps.concat([{ title: '', body: '', image: '' }]) })
+  },
+
+  chooseCover() {
+    wx.chooseMedia({
+      count: 1,
+      mediaType: ['image'],
+      success: (res) => this.setData({ cover: res.tempFiles[0].tempFilePath, uploadError: '' }),
+      fail: () => this.setData({ uploadError: '图片上传失败，请重试。' }),
+    })
+  },
+
+  saveDraft() {
+    wx.showToast({ title: '已保存草稿', icon: 'success' })
+  },
+
+  publish() {
+    if (!this.data.name.trim()) {
+      wx.showToast({ title: '先写菜名', icon: 'none' })
+      return
+    }
+    wx.showToast({ title: '已发布到动态', icon: 'success' })
+    setTimeout(() => wx.switchTab({ url: '/pages/feed/feed' }), 500)
   },
 })
