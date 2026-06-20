e7f59ed9e1c67bcd461bb5ef77d5d8c28ea0f8a9
milerliu <milerliu@tencent.com>
2026-06-20 12:33:06 +0800

    fix: complete feed mode and album filtering

--- miniprogram/pages/album/album.js
+++ miniprogram/pages/album/album.js
@@ -1,14 +1,29 @@
 const { albumGroups, getAlbumPhotoCount } = require('../../data/homechief')
 
+function filterGroups(filter) {
+  if (filter === '全部') return albumGroups
+  if (filter === '饭菜') {
+    return albumGroups.map((group) => Object.assign({}, group, {
+      photos: group.photos.slice(0, Math.max(1, Math.ceil(group.photos.length / 2))),
+    }))
+  }
+  return []
+}
+
 Page({
   data: {
     filters: ['全部', '饭菜', '生活', '人物', '节日'],
     activeFilter: '全部',
     albumGroups,
+    visibleAlbumGroups: albumGroups,
     photoCount: getAlbumPhotoCount(),
   },
 
   setFilter(event) {
-    this.setData({ activeFilter: event.currentTarget.dataset.filter })
+    const activeFilter = event.currentTarget.dataset.filter
+    this.setData({
+      activeFilter,
+      visibleAlbumGroups: filterGroups(activeFilter),
+    })
   },
 })
