8488881a9ea128fec65f43000112f020bae9a36a
milerliu <milerliu@tencent.com>
2026-06-20 09:40:45 +0800

    feat: add recipe library and detail

--- miniprogram/pages/recipe-detail/recipe-detail.js
+++ miniprogram/pages/recipe-detail/recipe-detail.js
@@ -1,5 +1,29 @@
+const { findRecipeById, getPostsByRecipeId } = require('../../data/homechief')
+
 Page({
   data: {
-    title: '菜谱详情',
+    recipe: null,
+    relatedPosts: [],
+  },
+
+  onLoad(query) {
+    const recipe = findRecipeById(query.id)
+    if (!recipe) {
+      wx.showToast({ title: '菜谱不存在', icon: 'none' })
+      setTimeout(() => wx.navigateBack(), 600)
+      return
+    }
+    this.setData({
+      recipe,
+      relatedPosts: getPostsByRecipeId(recipe.id),
+    })
+  },
+
+  cookAgain() {
+    wx.navigateTo({ url: `/pages/create-life/create-life?recipeId=${this.data.recipe.id}` })
+  },
+
+  editRecipe() {
+    wx.navigateTo({ url: `/pages/create-recipe/create-recipe?id=${this.data.recipe.id}` })
   },
 })
--- miniprogram/pages/recipes/recipes.js
+++ miniprogram/pages/recipes/recipes.js
@@ -1,5 +1,38 @@
+const { recipes } = require('../../data/homechief')
+
 Page({
   data: {
-    title: '菜谱',
+    query: '',
+    activeFilter: '全部',
+    filters: ['全部', '最近做过', '家常菜', '快手菜', '孩子爱吃', '待完善'],
+    recipes,
+    visibleRecipes: recipes,
+  },
+
+  onSearch(event) {
+    this.setData({ query: event.detail.value }, this.applyFilters)
+  },
+
+  setFilter(event) {
+    this.setData({ activeFilter: event.currentTarget.dataset.filter }, this.applyFilters)
+  },
+
+  applyFilters() {
+    const query = this.data.query.trim()
+    const filter = this.data.activeFilter
+    const visibleRecipes = recipes.filter((recipe) => {
+      const matchesQuery = !query || recipe.name.includes(query) || recipe.ingredients.join('').includes(query) || recipe.note.includes(query)
+      const matchesFilter = filter === '全部' || filter === '最近做过' || recipe.tags.includes(filter)
+      return matchesQuery && matchesFilter
+    })
+    this.setData({ visibleRecipes })
+  },
+
+  goDetail(event) {
+    wx.navigateTo({ url: `/pages/recipe-detail/recipe-detail?id=${event.currentTarget.dataset.id}` })
+  },
+
+  goCreateRecipe() {
+    wx.navigateTo({ url: '/pages/create-recipe/create-recipe' })
   },
 })
